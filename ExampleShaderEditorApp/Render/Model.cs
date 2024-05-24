d62ea3b46f3c98d51b4b0ce1401b8f748a92760b
ZackHuang0513 <zackhuang0513@gmail.com>
2024-05-24 17:58:31 +0800

    Remove unnecessary example

--- ExampleShaderEditorApp/Render/Model.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExampleShaderEditorApp.Render
-{
-    public class Model
-    {
-        public Mesh Mesh { get; set; }
-        public Texture Texture { get; set; }
-        public ShaderProgram Shader { get; set; }
-    }
-}
